40fa6187746e7241116b6e3443671bea65a6e7f8
Barry Luijbregts <b.luijbregts@waardedoorit.nl>
2019-04-24 16:45:44 +0200

    Code with container support

--- NationalCookies.MVC/Controllers/HomeController.cs
+++ NationalCookies.MVC/Controllers/HomeController.cs
@@ -1,14 +1,22 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace NationalCookies.Controllers
 {
     public class HomeController : Controller
     {
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                var request = new System.Net.Http.HttpRequestMessage();
+                request.RequestUri = new Uri("http://nationalcookies.api/api/values/");
+                var response = await client.SendAsync(request);
+                ViewData["Message"] += "The current time is: " + await response.Content.ReadAsStringAsync();
+            }
+
             return View();
         }
 
